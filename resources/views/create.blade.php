b8d9b80c02c70776eb84b75545730e5c923c3546
xvqiuu <132545168+xvqiuu@users.noreply.github.com>
2023-10-31 17:23:22 +0700

    second commit

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -5,7 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.2/dist/css/bootstrap.min.css">
-    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.16/dist/tailwind.min.css" rel="stylesheet">
     <title>Create</title>
 </head>
 <body style="font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif; background-color: #0D617D;  color: white;">
