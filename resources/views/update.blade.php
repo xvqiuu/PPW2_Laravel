beaed76e202519476e474837938108f05ddce6f1
xvqiuu <132545168+xvqiuu@users.noreply.github.com>
2023-10-31 17:28:56 +0700

    second commit

--- resources/views/update.blade.php
+++ resources/views/update.blade.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.2/dist/css/bootstrap.min.css">
-    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.16/dist/tailwind.min.css" rel="stylesheet">
+    
     <title>Update</title>
 </head>
 <body style="font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif; background-color: #0D617D;  color: white;" class="container-sm">
